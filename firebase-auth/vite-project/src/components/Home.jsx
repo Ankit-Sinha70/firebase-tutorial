b3591b087246f393494f92be9d782c5f53ee53ef
Ankit Sinha <ankits.brainerhub@gmail.com>
2024-09-12 15:01:55 +0530

    Added firebase auth code

--- firebase-auth/vite-project/src/components/Home.jsx
+++ firebase-auth/vite-project/src/components/Home.jsx
@@ -1,26 +1,105 @@
-import React from 'react'
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { auth } from "../firebase-config";
 
 function Home() {
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [user, setUser] = useState({});
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const registerUser = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+      setRegisterEmail("")
+      setRegisterPassword("")
+      alert("User register successfully")
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const handleLogOut = async () => {
+    await signOut(auth);
+  };
+
+  const loginUser = async () => {
+    try {
+      await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      setIsLoggedIn(true);
+      setLoginEmail("");
+      setLoginPassword("");
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <>
-        <div>
-            <h2>Register User</h2>
-            <input type="text" placeholder="email..." />
-            <input type="text" placeholder='password...'/>
-            <button>Create User</button>
-        </div>
-        <div>
-            <h2>LogIn</h2>
-            <input type="text" placeholder='email..' />
-            <input type="text" placeholder='password..' />
-            <button>Login</button>
-        </div>
-        <div>
-            <h2>User Logged In : </h2>
-            <button>SignOut</button>
-        </div>
+      <div>
+        <h2>Register User</h2>
+        <input
+          type="text"
+          placeholder="email..."
+          value={registerEmail}
+          onChange={(e) => setRegisterEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="password..."
+          value={registerPassword}
+          onChange={(e) => setRegisterPassword(e.target.value)}
+        />
+        <button onClick={registerUser}>Create User</button>
+      </div>
+      <div>
+        <h2>LogIn</h2>
+        <input
+          type="text"
+          placeholder="email.."
+          value={loginEmail}
+          onChange={(e) => setLoginEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="password.."
+          value={loginPassword}
+          onChange={(e) => setLoginPassword(e.target.value)}
+        />
+        <button onClick={loginUser}>Login</button>
+      </div>
+      <div>
+      {isLoggedIn && user && (
+          <>
+            <h3>Logged in as: {user.email}</h3>
+            <button onClick={handleLogOut}>Log Out</button>
+          </>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- firebase-auth/vite-project/src/firebase-config.js
+++ firebase-auth/vite-project/src/firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // Firebase configuration using environment variables
 const firebaseConfig = {
@@ -14,4 +14,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const auth = getAuth(app) 
