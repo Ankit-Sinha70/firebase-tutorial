300f51f2040ed5dde93282289eef93d3bcb839af
Ankit Sinha <ankits.brainerhub@gmail.com>
2024-09-13 16:27:16 +0530

    added the delete functionality

--- firebase-blog/src/pages/Home.jsx
+++ firebase-blog/src/pages/Home.jsx
@@ -1,6 +1,7 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db } from "../firebase-config";
+import { IoTrashBinSharp } from "react-icons/io5";
 
 function Home() {
   const [postList, setPostList] = useState([]);
@@ -12,6 +13,17 @@ function Home() {
     };
     getPost();
   }, []);
+
+  const handleDeletePost = async (id) => {
+    try {
+      const postDoc = doc(db, "posts", id);
+      await deleteDoc(postDoc);
+      setPostList((prevPosts) => prevPosts.filter((post) => post.id !== id));
+      alert("Data deleted successfully")
+    } catch (error) {
+      console.error("Error deleting post: ", error);
+    }
+  };
   return (
     <div className="homePage">
       {postList.map((item) => {
@@ -20,6 +32,11 @@ function Home() {
             <div className="postHeader">
               <div className="title">
                 <h1>{item.title}</h1>
+              </div>
+              <div className="deletePost">
+                <button onClick={() => handleDeletePost(item.id)}>
+                  <IoTrashBinSharp style={{ color: "gray" }} />
+                </button>
               </div>
             </div>
             <div className="postTextContainer">{item.postText}</div>
