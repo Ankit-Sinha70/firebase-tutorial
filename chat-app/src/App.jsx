8810696d68272371d371fcac35ea16eb981cfaf8
Ankit Sinha <ankits.brainerhub@gmail.com>
2024-09-17 20:01:34 +0530

    add signOut

--- chat-app/src/App.jsx
+++ chat-app/src/App.jsx
@@ -3,12 +3,21 @@ import "./App.css";
 import { Auth } from "./components/Auth";
 import Cookies from "universal-cookie";
 import Chat from "./components/Chat";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
 const cookies = new Cookies();
 
 function App() {
   const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
   const [room, setRoom] = useState(null);
   const roomInputRef = useRef();
+
+  const handleSignOutUser = async() => {
+    await signOut(auth);
+    cookies.remove("auth-token");
+    setIsAuth(false);
+    setRoom(null)
+  }
 
   if (!isAuth) {
     return (
@@ -33,7 +42,7 @@ function App() {
       )}
 
       <div className="sign-out">
-        <button>SignOut</button>
+        <button onClick={handleSignOutUser}>SignOut</button>
       </div>
     </>
   );
