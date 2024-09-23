06f4d8aa4d80832f8d514a6e406d442adcb7fc23
Ankit Sinha <ankits.brainerhub@gmail.com>
2024-09-23 18:10:03 +0530

    Added code

--- expanse-tracker/src/hooks/useGetTransactions.js
+++ expanse-tracker/src/hooks/useGetTransactions.js
@@ -9,7 +9,12 @@ import { useEffect, useState } from "react";
 import { db } from "../config/firebase";
 import { useGetUserInfo } from "./useGetUserInfo";
 export const useGetTransaction = () => {
-  const [transactions, setTransactions] = useState("");
+  const [transactions, setTransactions] = useState([]);
+  const [transactionTotal, setTransactionTotal] = useState({
+    balance: 0.0,
+    income: 0.0,
+    expense: 0.0,
+  });
   const transactionCollectionRef = collection(db, "transactions");
   const { userId } = useGetUserInfo();
 
@@ -22,13 +27,27 @@ export const useGetTransaction = () => {
       );
       onSnapshot(queryCollection, (snapShot) => {
         let docs = [];
+        let totalIncome = 0;
+        let totalExense = 0;
         snapShot.forEach((doc) => {
           const data = doc.data();
           const id = doc.id;
-
           docs?.push({ ...data, id });
+          if (data.transactionType === "expense") {
+            totalExense += Number(data.transactionAmount);
+          } else {
+            totalIncome += Number(data.transactionAmount);
+          }
+
         });
         setTransactions(docs);
+
+        let balance = totalIncome - totalExense
+        setTransactionTotal({
+          balance, 
+          income: totalIncome,
+          expense: totalExense
+        })
       });
     } catch (error) {
       console.error(error);
@@ -39,5 +58,5 @@ export const useGetTransaction = () => {
     getTransaction();
   }, []);
 
-  return { transactions };
+  return { transactions, transactionTotal };
 };
--- expanse-tracker/src/pages/expense-tracker/index.jsx
+++ expanse-tracker/src/pages/expense-tracker/index.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
 import useAddTransaction from "../../hooks/useAddTransaction";
 import { useGetTransaction } from "../../hooks/useGetTransactions";
+import { useGetUserInfo } from "../../hooks/useGetUserInfo";
 import "./styles.css";
+import { signOut } from "firebase/auth";
+import { auth } from "../../config/firebase";
+import { useNavigate } from "react-router-dom";
 
 export const ExpenseTracker = () => {
   const [description, setDescription] = useState("");
   const [transactionAmount, setTransactionAmount] = useState(0);
   const [transactionType, setTransactionType] = useState("expense");
   const { addTransaction } = useAddTransaction();
-  const { transactions } = useGetTransaction();
-  console.log("transactions: ", transactions);
+  const { transactions, transactionTotal } = useGetTransaction();
+  const { name, profile } = useGetUserInfo();
+  const { balance, income, expense } = transactionTotal;
+  console.log("profile: ", profile);
+  const navigate = useNavigate();
 
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -18,22 +25,54 @@ export const ExpenseTracker = () => {
     setTransactionAmount("");
   };
 
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      localStorage.clear();
+      setTimeout(() => {
+        navigate("/");
+      }, 1500);
+    } catch (error) {
+      console.error(error, "Error while sign-out");
+    }
+  };
+
   return (
     <div className="expense-tracker">
       <div className="container">
-        <h1>Expense Tracker</h1>
+        <h1>{name}'s Expense Tracker</h1>
+        {profile && (
+          <div className="profile">
+            <img
+              className="profile-photo"
+              src={profile.toString()}
+              alt={"profilePhoto"}
+              height={100}
+              width={100}
+              style={{ borderRadius: "50%" }}
+            />
+          </div>
+        )}
         <div className="balance">
-          <h3>Your Balance</h3>
-          <h2>$0.00</h2>
+          <h3>Your Balance</h3>$ {balance}{" "}
+          {balance >= 0 ? (
+            <span role="img" aria-label="smile">
+              😊
+            </span>
+          ) : (
+            <span role="img" aria-label="sad">
+              😢
+            </span>
+          )}
         </div>
         <div className="summary">
           <div className="income">
             <h4>Income</h4>
-            <p>$0.00</p>
+            <p>$ {income}</p>
           </div>
           <div className="expenses">
             <h4>Expense</h4>
-            <p>$0.00</p>
+            <p>$ {expense}</p>
           </div>
         </div>
         <form className="add-transaction" onSubmit={onSubmit}>
@@ -54,28 +93,44 @@ export const ExpenseTracker = () => {
             onChange={(e) => setTransactionAmount(e.target.value)}
           />
           <br />
-          <input
-            type="radio"
-            id="expense"
-            value="expense"
-            checked={transactionType === "expense"}
-            onChange={(e) => setTransactionType(e.target.value)}
-          />
-          <label>Expense</label>
-          <input
-            type="radio"
-            id="income"
-            value="income"
-            checked={transactionType === "income"}
-            onChange={(e) => setTransactionType(e.target.value)}
-          />
-          <label>Income</label>
+          <div className="radio-group">
+            <div>
+              <input
+                type="radio"
+                id="expense"
+                value="expense"
+                checked={transactionType === "expense"}
+                onChange={(e) => setTransactionType(e.target.value)}
+              />
+              <label htmlFor="expense">Expense</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="income"
+                value="income"
+                checked={transactionType === "income"}
+                onChange={(e) => setTransactionType(e.target.value)}
+              />
+              <label htmlFor="income">Income</label>
+            </div>
+          </div>
+
           <br />
           <br />
-          <button type="submit">Add Transaction</button>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              gap: "20px",
+            }}
+          >
+            <button type="submit">Add Transaction</button>
+            <button onClick={handleSignOut}>SignOut</button>
+          </div>
         </form>
       </div>
-        {/* <h3>Transactions</h3> */}
+
       <div className="transactions">
         <ul>
           {transactions &&
